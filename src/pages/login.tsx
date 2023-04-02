3684e593c117625d0c2be4570a78e46f40cf14f7
Murtaza Muffazal Dhariwala <85226002+MurtazaD1410@users.noreply.github.com>
2023-04-03 01:54:23 +0530

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -21,7 +21,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          client_id:"936661147855-ncuddqdaoefb7a0cgcbk2l1t4c5q9994.apps.googleusercontent.com",
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
